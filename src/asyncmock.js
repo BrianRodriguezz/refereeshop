4abe580c36314b53778ed1746a6db8c2704914d8
Brianjr <brian_rodriguez94@hotmail.com>
2023-07-28 23:22:35 -0300

    final

---
+++ src/asyncmock.js
@@ -0,0 +1,37 @@
+export const misProductos = [
+    { id: "1", nombre: "Remera", precio: 1500, img: "../img/remera.jpg", idCat: "2" },
+    { id: "2", nombre: "Remera Entrenamiento", precio: 1200, img: "../img/remera e.jpg", idCat: "2" },
+    { id: "3", nombre: "Short", precio: 1180, img: "../img/short.jpg", idCat: "3" },
+    { id: "4", nombre: "Medias", precio: 1900, img: "../img/medias.jpg", idCat: "3" },
+]
+export const getProductos = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(misProductos);
+        }, 100)
+    })
+}
+
+
+//Creamos una nueva función similar a la anterior pero qu enos retorne un solo item: 
+
+export const getUnProducto = (id) => {
+    return new Promise(resolve => {
+        setTimeout( () => {
+            const producto = misProductos.find(prod=> prod.id === id);
+            resolve(producto);
+        }, 100)
+    })
+}
+
+
+//Creamos una función que retora un array de una determinada categoría de producto: 
+
+export const getProductosPorCategoria = (idCategoria) => {
+    return new Promise ( resolve => {
+        setTimeout( () => {
+            const productosCategoria = misProductos.filter(prod => prod.idCat === idCategoria);
+            resolve(productosCategoria);
+        }, 100 )
+    })
+}
---
+++ src/services/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "tp-final-80ed7.firebaseapp.com",
+  projectId: "tp-final-80ed7",
+  storageBucket: "tp-final-80ed7.appspot.com",
+  messagingSenderId: "1063213143004",
+  appId: "1:1063213143004:web:02b43bfa43e0718994f519"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+
+
+
